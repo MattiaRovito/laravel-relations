f4b7fa0d59c6ae09152b4c2bc39a4fa6da803689
MattiaR <rovitomattia91@gmail.com>
2021-09-15 14:21:42 +0200

    creato le relazioni in Category e Post

--- app/Category.php
+++ app/Category.php
@@ -9,7 +9,7 @@ class Category extends Model
     public function posts(){
         // il $this indica proprio la categoria, in questo caso Category.
 
-        // N.B.: belongsTo-> singolare (infatti category)
-        return $this->belongsTo('App\Category');
+        // N.B.: hasMany-> plurale (infatti posts). Ovvero, una categoria può avere molti post.
+        return $this->hasMany('App\Post');
     }
 };
--- app/Post.php
+++ app/Post.php
@@ -9,6 +9,15 @@ class Post extends Model
     protected $fillable = [
         'title',
         'slug',
-        'content'
+        'content',
+        // aggiungo la category_id
+        'category_id'
     ];
+
+    public function category(){
+        // il $this indica proprio la categoria, in questo caso Posts.
+
+        // N.B.: belongsTo-> singolare (infatti category). Ovvero, un determinato post è scritto da un solo utente.
+        return $this->belongsTo('App\Category');
+    }
 }
